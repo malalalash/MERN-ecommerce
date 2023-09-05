f44987146d1d477c704b3343eb8b829dbccfa9eb
Mikołaj Malarczyk <hcmiki358@gmail.com>
2023-09-05 23:44:16 +0200

    added hero image

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,22 @@
+import salemd from "../assets/images/salemd.jpg";
+import salesm from "../assets/images/salesm.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full min-h-screen">
+      <div className="w-full h-64 sm:h-80 lg:h-96 bg-black/50">
+        <picture>
+          <source srcSet={salesm} media="(max-width: 768px)" />
+          <source srcSet={salemd} />
+          <img
+            src={salemd}
+            alt="sale image"
+            className="w-full h-full object-cover"
+          />
+        </picture>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
