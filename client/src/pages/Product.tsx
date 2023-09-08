b2a81f97fe2487f39d0f38e57841676f8b0060d7
Mikołaj Malarczyk <hcmiki358@gmail.com>
2023-09-08 12:59:03 +0200

    added more styling

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -26,72 +26,75 @@ const Product = () => {
               <img
                 src={product?.imageUrl}
                 alt={product?.name}
-                className="w-full max-h-96 object-cover"
+                className="w-full max-h-96 lg:max-h-[450px] object-cover"
               />
             </div>
-            <div className="bg-white h-full lg:min-h-96 flex flex-col justify-between">
+            <article className="bg-white h-full flex flex-col justify-between">
               <div className="border-b pb-2">
-                <h1 className="text-2xl font-extrabold">{product?.name}</h1>
+                <h1 className="text-3xl md:text-4xl mb-1 font-extrabold">{product?.name}</h1>
                 <span>{product?.category}</span>
               </div>
-              <p className="my-3 tracking-wide leading-tighter font-mono break-words">
-                {product?.description}
-              </p>
               <span className="text-2xl font-extrabold">
                 ${product?.price?.toFixed(2)}
               </span>
-              <div className="flex items-center gap-5 mt-2">
-                <p className="lg:text-lg">Select color:</p>
-                {product?.colors.map((color, c) => {
-                  return (
-                    <button
-                      value={color}
-                      key={c}
-                      className="p-2.5 lg:p-3 rounded-full hover:outline outline-2"
-                      style={{ background: color }}
-                    ></button>
-                  );
-                })}
-              </div>
-              <div className="flex items-center gap-3 my-2">
-                <label htmlFor="quantity" className="lg:text-lg">
-                  Quantity:
+              <p className="tracking-wide leading-tighter font-mono break-words my-5 md:m-0">
+                {product?.description}
+              </p>
+              <div className="gap-2 flex flex-col">
+                <div className="flex items-center gap-5">
+                  <p className="lg:text-lg">Select color:</p>
+                  {product?.colors.map((color, c) => {
+                    return (
+                      <button
+                        value={color}
+                        key={c}
+                        className="p-2.5 lg:p-3 rounded-full hover:outline outline-2"
+                        style={{ background: color }}
+                      ></button>
+                    );
+                  })}
+                </div>
+                <div className="flex items-center gap-3">
+                  <label htmlFor="quantity" className="lg:text-lg">
+                    Quantity:
+                  </label>
+                  <input
+                    type="number"
+                    name="quantity"
+                    id="quantity"
+                    defaultValue={1}
+                    min={1}
+                    max={10}
+                    className="border border-black"
+                  />
+                </div>
+
+                <label className="text-lg" htmlFor="size">
+                  Select size:
                 </label>
-                <input
-                  type="number"
-                  name="quantity"
-                  id="quantity"
-                  defaultValue={1}
-                  min={1}
-                  max={10}
-                  className="border border-black"
-                />
+                <select
+                  className="w-full border border-black p-2 cursor-pointer lg:text-lg"
+                  name="size"
+                  id="size"
+                >
+                  {product?.sizes.map((size, s) => {
+                    return (
+                      <option key={s} className="lg:text-lg">
+                        {size}
+                      </option>
+                    );
+                  })}
+                </select>
+                <div className="flex items-center gap-3">
+                  <button className="text-lg w-full py-2 uppercase font-semibold text-white bg-black hover:bg-gray-200 hover:text-black border border-black transition focus:text-black focus:bg-gray-200">
+                    Add to cart
+                  </button>
+                  <button className="border transition border-black p-2 hover:bg-gray-200 focus:bg-gray-200">
+                    <HeartIcon className="h-7 w-7" />
+                  </button>
+                </div>
               </div>
-              <label className="text-lg" htmlFor="size">
-                Select size:
-              </label>
-              <select
-                className="w-full border border-black p-2 mt-2 cursor-pointer lg:text-lg"
-                name="size"
-                id="size"
-              >
-                {product?.sizes.map((size, s) => {
-                  return (
-                    <option key={s} className="lg:text-lg">
-                      {size}
-                    </option>
-                  );
-                })}
-              </select>
-              <div className="flex items-center gap-3 mt-2">
-                <button className="text-lg w-full py-2 uppercase font-semibold text-white bg-black hover:bg-gray-200 hover:text-black border border-black transition focus:text-black focus:bg-gray-200">
-                  Add to cart
-                </button>
-                <button className="border transition border-black p-2 hover:bg-gray-200 focus:bg-gray-200">
-                  <HeartIcon className="h-7 w-7" />
-                </button>
-              </div>
-            </div>
+            </article>
           </div>
         </div>
       </article>
