fced78295eb0b76821eac8465409e725e1d16dd5
Mikołaj Malarczyk <hcmiki358@gmail.com>
2023-09-20 23:37:33 +0200

    added chart/recharts 2

---
+++ admin/src/pages/Home/Chart.tsx
@@ -0,0 +1,59 @@
+import {
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer,
+  } from "recharts";
+  
+  const data = [
+    {
+      name: "Mon",
+      sales: "10",
+    },
+    {
+      name: "Tue",
+      sales: "22",
+    },
+    {
+      name: "Wed",
+      sales: "34",
+    },
+  
+    {
+      name: "Thu",
+      sales: "56",
+    },
+  
+    {
+      name: "Fri",
+      sales: "78",
+    },
+  
+    {
+      name: "Sat",
+      sales: "12",
+    },
+    {
+      name: "Sun",
+      sales: "34",
+    },
+  ];
+  
+  export const renderLinneChart = (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart data={data} margin={{ left: -30, top: 19 }}>
+        <Line type="monotone" dataKey="sales" stroke="#1810ad" strokeWidth={2} />
+        <CartesianGrid stroke="#ccc" vertical={false} />
+        <XAxis
+          dataKey="name"
+          stroke="#000000"
+          padding={{ left: 20, right: 20 }}
+        />
+        <YAxis stroke="#000000" />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
--- admin/src/pages/Home/Home.tsx
+++ admin/src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import Boxes from "./Boxes";
+import { renderLinneChart } from "./Chart";
 
 const Home = () => {
   return (
     <div className="grid grid-cols-1 gap-5">
       <Boxes />
-      <div className="bg-white">xd</div>
+      <div className="bg-white p-5 pb-10 h-[250px] sm:h-[350px] text-xs sm:text-base max-w-3xl">
+        <span className="text-lg">Sales</span>
+        {renderLinneChart}
+      </div>
     </div>
   );
 };
